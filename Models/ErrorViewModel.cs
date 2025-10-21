d6fa48a1e1991a9365fdae2f57eb0aa5de46b10c
Nguyen Nam Khanh <nguyennamkhanhnd2004@gmail.com>
2025-10-21 23:35:15 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AssociationPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
